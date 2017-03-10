bbede2006e0453b674a366540ea54dec09c0cd9c
ikorolkova <ikorolkovad@gmail.com>
2017-03-10 09:54:37 +0300

    delete mock-contacts

--- src/app/mock-contacts.ts
+++
@@ -1,7 +0,0 @@
-import { Contact} from './contact';
-
-export const CONTACTS: Contact[] =[
-  {id: 1, name: 'TestName1'},
-  {id: 2, name: 'TestName2'},
-  {id: 3, name: 'TestName3'},
-]
--- src/app/modules/app.module.ts
+++ src/app/modules/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule}      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpModule} from '@angular/http';
 
 import { AppComponent }  from '../components/app.component';
 import { ContactDetailComponent } from '../components/contact-detail.component';
 import { ContactService } from '../services/contact.service';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule],
+  imports:      [ BrowserModule, FormsModule, HttpModule],
   declarations: [ AppComponent, ContactDetailComponent],
   bootstrap:    [ AppComponent ],
   providers: [ContactService]
